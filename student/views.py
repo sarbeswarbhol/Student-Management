dc826769cf012b9a667d7b7ecdb957e40223397f
Sarbeswar <sarbeswar.bhol.04@gmail.com>
2025-05-30 22:51:51 +0530

    Enhance student management functionality
    Updated models, views, and templates to improve student management features. Added CRUD operations for students, refined gender field in models, and integrated dynamic data rendering in templates. Removed unused student images and added a new image file.

--- student/views.py
+++ student/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from student.models import Parent, Student
 
@@ -43,13 +44,6 @@ def add_student(request):
             present_location=present_location,
             permanent_address=permanent_address,
         )
-        from django.utils.text import slugify
-        base_slug = slugify(f"{first_name} {last_name}")
-        unique_slug = base_slug
-        counter = 1
-        while Student.objects.filter(slug=unique_slug).exists():
-            unique_slug = f"{base_slug}-{counter}"
-            counter += 1
 
         #save student info
         student = Student.objects.create(
@@ -58,15 +52,14 @@ def add_student(request):
             student_id=student_id,
             gender=gender,
             date_of_birth=date_of_birth,
-            joining_date=joining_date,
+            joining_date=joining_date ,
             student_class=student_class,
             religion=religion,
             mobile_number=mobile_number,
             admission_number=admission_number,
             section=section,
             student_image=student_image,
-            parent=parent,
-            slug=unique_slug
+            parent=parent
         )
         messages.success(request, "Student added successfully!")
         # return render(request, 'student_list')   
@@ -75,10 +68,78 @@ def add_student(request):
 
 
 def student_list(request):
-    return render(request, 'students/student.html')
+    student_list = Student.objects.select_related("parent").all()
+    context = {
+        'student_list': student_list
+    }
+    return render(request, 'students/students.html', context)
 
-def edit_student(request):
-    return render(request, 'students/edit-student.html')
+def edit_student(request, slug):
+    student = get_object_or_404(Student, slug=slug)
+    parent = student.parent if hasattr(student, 'parent') else None
+    # print(parent)
+    # print(student)
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        student_id = request.POST.get('student_id')
+        gender = request.POST.get('gender')
+        date_of_birth = request.POST.get('date_of_birth')
+        student_class = request.POST.get('student_class')
+        religion = request.POST.get('religion')
+        mobile_number = request.POST.get('mobile_number')
+        admission_number = request.POST.get('admission_number')
+        section = request.POST.get('section')
+        joining_date = request.POST.get('joining_date')
+        student_image = request.FILES.get('student_image')  if request.FILES.get('student_image') else student.student_image
 
-def view_student(request):
-    return render(request, 'students/student-details.html')
+        
+        #handle parent data
+        parent.father_name = request.POST.get('father_name')
+        parent.father_occupation = request.POST.get('father_occupation')
+        parent.father_mobile = request.POST.get('father_mobile')
+        parent.father_email = request.POST.get('father_email')
+        parent.mother_name = request.POST.get('mother_name')
+        parent.mother_occupation = request.POST.get('mother_occupation')
+        parent.mother_mobile = request.POST.get('mother_mobile')
+        parent.mother_email = request.POST.get('mother_email')
+        parent.present_location = request.POST.get('present_location')
+        parent.permanent_address = request.POST.get('permanent_address')
+        parent.save()
+        
+        #save student info
+
+        student.first_name=first_name
+        student.last_name=last_name
+        student.student_id=student_id
+        student.gender=gender
+        student.date_of_birth=date_of_birth
+        student.joining_date=joining_date 
+        student.student_class=student_class
+        student.religion=religion
+        student.mobile_number=mobile_number
+        student.admission_number=admission_number
+        student.section=section
+        student.student_image=student_image
+        student.save()
+
+        
+        return redirect('student_list')  
+    return render(request, 'students/edit-student.html', {'student': student, 'parent': parent})
+
+def view_student(request, slug):
+    student = get_object_or_404(Student, student_id = slug)
+    context = {
+        'student': student
+    }
+    return render(request, 'students/student-details.html', context)
+
+
+def delete_student(request, slug):
+    if request.method == "POST":
+        student = get_object_or_404(Student, slug=slug)
+        student_name = f"{student.first_name}{student.last_name}"
+        student.delete()
+        
+        return redirect ('student_list')
+    return HttpResponseForbidden()
