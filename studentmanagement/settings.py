96538e4d6a15915ece134e4d6066321f73c9ecae
Sarbeswar <sarbeswar.bhol.04@gmail.com>
2025-05-31 00:05:21 +0530

    Add home_auth app and update settings/urls
    Introduced a new Django app 'home_auth' with initial files including models, views, admin, and tests. Updated 'studentmanagement/settings.py' to include 'home_auth' in INSTALLED_APPS and modified 'studentmanagement/urls.py' to add a route for 'home_auth.urls'.

--- studentmanagement/settings.py
+++ studentmanagement/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "school",
-    "student"
+    "student",
+    "home_auth",
 ]
 
 MIDDLEWARE = [
--- studentmanagement/urls.py
+++ studentmanagement/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('school.urls')),
     path('student/', include('student.urls')),
+    path('authentication/', include('home_auth.urls')),
 ]
